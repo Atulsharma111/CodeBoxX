8d298437b3ea195d95dd96136bc1a61df354bfef
atulsharma111 <atulsharma61407@gmail.com>
2026-03-09 16:04:35 +0530

    chapter 1and 2 project setup

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,10 @@ import Image from "next/image";
 export default function Home() {
   return (
 
-
+/**
+ * default page of application 
+ * @returns {JSX.Element} - The home page component
+ */
 
     <div>
        <h1 className="font-game">this is my home page this is my first page </h1>
